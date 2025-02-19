cf3dab27062f6298cf925396016c3d061d9528b8
Song, Gyu <songkyoo@hotmail.com>
2025-02-19 16:40:56 +0900

    Lens.Tests 프로젝트를 테스트 프로젝트로 변경

---
+++ Lens.Tests/LensOfTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Macaron.Optics.Tests;
+
+public class LensOfTests
+{
+    public sealed record Person(string Name, int Age);
+
+    [Fact]
+    public void LensOf_WithRecordType_GenerateLensExtensionMethodsOfRecord()
+    {
+        var lensOfPerson = Lens.Of<Person>();
+        var person = new Person("Alice", 7);
+
+        Assert.Equal("Alice", lensOfPerson.Name().Get(person));
+        Assert.Equal(7, lensOfPerson.Age().Get(person));
+    }
+}

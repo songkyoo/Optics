98d116a15a5555d19b5eeecffc5589141fee848a
Song, Gyu <songkyoo@hotmail.com>
2025-02-22 23:57:11 +0900

    OptionLens<Option<TValue1>, TValue2> 형식으로 연결되는 Compose 확장 메서드 추가

--- Lens/LensExtensions.cs
+++ Lens/LensExtensions.cs
@@ -117,6 +117,60 @@ public static class LensExtensions
         );
     }
 
+    public static OptionLens<T, TValue2> Compose<T, TValue1, TValue2>(
+        this OptionLens<T, TValue1> lens1,
+        OptionLens<Option<TValue1>, TValue2> lens2
+    )
+    {
+        return OptionLens<T, TValue2>.Of(
+            getter: source =>
+            {
+                var value0 = source;
+                var value1 = lens1.Get(value0);
+                var value2 = lens2.Get(value1);
+
+                return value2;
+            },
+            setter: (source, value) =>
+            {
+                var value0 = source;
+                var value1 = lens1.Get(value0);
+
+                var newValue1 = lens2.Set(value1, value);
+                var newValue0 = newValue1.IsSome ? lens1.Set(source, newValue1.Value): source;
+
+                return newValue0;
+            }
+        );
+    }
+
+    public static OptionLens<Option<T>, TValue2> Compose<T, TValue1, TValue2>(
+        this OptionLens<Option<T>, TValue1> lens1,
+        OptionLens<Option<TValue1>, TValue2> lens2
+    )
+    {
+        return OptionLens<Option<T>, TValue2>.Of(
+            getter: source =>
+            {
+                var value0 = source;
+                var value1 = lens1.Get(value0);
+                var value2 = lens2.Get(value1);
+
+                return value2;
+            },
+            setter: (source, value) =>
+            {
+                var value0 = source;
+                var value1 = lens1.Get(value0);
+
+                var newValue1 = lens2.Set(value1, value);
+                var newValue0 = newValue1.IsSome ? lens1.Set(source, newValue1.Value): source;
+
+                return newValue0;
+            }
+        );
+    }
+
     public static Lens<T, TValue2> Compose<T, TValue1, TValue2>(
         this OptionLens<T, TValue1> lens1,
         Lens<TValue1, TValue2> lens2,

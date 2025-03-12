1f3a9f1b6e9bde4eb92a5f48d0501e60ff001b82
Song, Gyu <songkyoo@hotmail.com>
2025-03-13 01:58:46 +0900

    누락된 변환 추가

--- Optics.Core/SetterExtensions.cs
+++ Optics.Core/SetterExtensions.cs
@@ -1,7 +1,41 @@
+using Macaron.Functional;
+
 namespace Macaron.Optics;
 
 public static class SetterExtensions
 {
+    public static Optional<T, TValue> ToOptional<T, TValue>(
+        this Setter<T, TValue> setter,
+        Func<T, Maybe<TValue>> optionalGetter
+    )
+    {
+        return Optional<T, TValue>.Of(optionalGetter, setter.Set);
+    }
+
+    public static Optional<T, TValue> ToOptional<T, TValue>(
+        this Setter<T, TValue> setter,
+        OptionalGetter<T, TValue> optionalGetter
+    )
+    {
+        return Optional<T, TValue>.Of(optionalGetter.Get, setter.Set);
+    }
+
+    public static Optional<T, TValue> ToOptional<T, TValue>(
+        this Setter<T, TValue> setter,
+        Func<T, TValue> getter
+    )
+    {
+        return Optional<T, TValue>.Of(getter, setter.Set);
+    }
+
+    public static Optional<T, TValue> ToOptional<T, TValue>(
+        this Setter<T, TValue> setter,
+        Getter<T, TValue> getter
+    )
+    {
+        return Optional<T, TValue>.Of(getter.Get, setter.Set);
+    }
+
     public static Lens<T, TValue> ToLens<T, TValue>(
         this Setter<T, TValue> setter,
         Func<T, TValue> getter

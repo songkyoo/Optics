af87e6c424cf9f9551f53a6cabf5b61487d82fa0
Song, Gyu <songkyoo@hotmail.com>
2025-03-13 23:34:21 +0900

    Constructor.Compose 메서드 추가

--- Optics.Core/Constructor.cs
+++ Optics.Core/Constructor.cs
@@ -1,6 +1,6 @@
 namespace Macaron.Optics;
 
-public static class Constructor
+public static partial class Constructor
 {
     #region Static
     public static Constructor<T, TValue> Of<T, TValue>(Func<TValue, T> constructor) => new(constructor);
